80e537174f245448dd4c8f986864f4a9a1a8728a
navraj703 <56917490+navraj703@users.noreply.github.com>
2020-08-21 07:18:48 +0530

    Update index.js

--- src/Firebase/index.js
+++ src/Firebase/index.js
@@ -1,14 +1,14 @@
 import * as firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCV74F0meaanwEYevLrEusuX6OBHQkYW84",
-    authDomain: "web-addmission.firebaseapp.com",
-    databaseURL: "https://web-addmission.firebaseio.com",
-    projectId: "web-addmission",
-    storageBucket: "web-addmission.appspot.com",
-    messagingSenderId: "785421443350",
-    appId: "1:785421443350:web:6df57db01779b7e0503037",
-    measurementId: "G-CTHRX84Q1T"
+    apiKey: "#######################",
+    authDomain: "####################",
+    databaseURL: "###################",
+    projectId: "#################",
+    storageBucket: "##################",
+    messagingSenderId: "##################",
+    appId: "###################",
+    measurementId: "#####################"
 };
  
 firebase.initializeApp(firebaseConfig);
